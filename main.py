90acd4d85ea78ab3eee52746fe0397b1ba43f1ac
VarunGonsalves <96880188+VarunGonsalves@users.noreply.github.com>
2023-11-03 17:59:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+def isfloat(num):
+    try:
+        float(num)
+        return True
+    except ValueError:
+        return False
+
 def count_batteries_by_health(present_capacities):  
   battery = {
     "invalid_entry": 0, 
@@ -6,6 +13,8 @@ def count_batteries_by_health(present_capacities):
     "failed": 0
   }
   for i in present_capacities:
+    if(str(type(i)) == "<class 'str'>"):
+        if(isfloat(i)): i = float(i)
     if(str(type(i)) == "<class 'int'>" or str(type(i)) == "<class 'float'>"):
         p = int(100*float(i))/120
         if(p > 100 or p < 0): battery["invalid_entry"] += 1
@@ -18,12 +27,12 @@ def count_batteries_by_health(present_capacities):
 
 def test_bucketing_by_health():
   print("Counting batteries by SoH...\n")
-  present_capacities = [150, 180, 119, 113, 101.5, 97, 96.5, 96,80, 75, 74.4, 51, -9, -17, 'o', ['a','e','i','o','u']]
+  present_capacities = [150, 180, 119, 113, 101.5, 97, 96.5, 96,80, 75, 74.4, 51, -9, -17, 'o', ['a','e','i','o','u'], '110', '130.5', '20.22', '80']
   counts = count_batteries_by_health(present_capacities)
-  assert(counts["invalid_entry"] == 6)
-  assert(counts["healthy"] == 5)
-  assert(counts["exchange"] == 3)
-  assert(counts["failed"] == 2)
+  assert(counts["invalid_entry"] == 7)
+  assert(counts["healthy"] == 6)
+  assert(counts["exchange"] == 4)
+  assert(counts["failed"] == 3)
   print("Done counting :)")
 
 

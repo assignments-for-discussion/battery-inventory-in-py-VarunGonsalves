ee831452637e4b8f303caa8a81e3d18f5912028a
VarunGonsalves <96880188+VarunGonsalves@users.noreply.github.com>
2023-11-03 17:31:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
-
 def count_batteries_by_health(present_capacities):  
   battery = {
+    "invalid": 0, 
     "healthy": 0,
     "exchange": 0,
     "failed": 0
   }
   for i in present_capacities:
-    p = 100*int(i)/120
-    if(p >= 80): battery["healthy"] += 1
-    elif(p >= 62): battery["exchange"] += 1
+    p = int(100*float(i))/120
+    if(p > 100 or p < 0): battery["invalid"] += 1
+    elif(p > 80): battery["healthy"] += 1
+    elif(p > 62): battery["exchange"] += 1
     else: battery["failed"] += 1
   return battery
 
+
 def test_bucketing_by_health():
   print("Counting batteries by SoH...\n")
-  present_capacities = [113, 116, 80, 95, 92, 70]
+  present_capacities = [150, 180, 119, 113, 101.5, 97, 96.5, 96,80, 75, 74.4, 51, -9, -17]
   counts = count_batteries_by_health(present_capacities)
-  assert(counts["healthy"] == 2)
+  assert(counts["invalid"] == 4)
+  assert(counts["healthy"] == 5)
   assert(counts["exchange"] == 3)
-  assert(counts["failed"] == 1)
+  assert(counts["failed"] == 2)
   print("Done counting :)")
 
 
